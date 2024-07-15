7d3a294d4d032601881332f44c2601aecd1aaa42
Letícia Bittencourt Vieira <bittencourt.vieira@academico.ifg.edu.br>
2024-07-15 17:59:03 -0300

    commit do segundo curso de php

---
+++ VARIAVEIS/var_de_var/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Podemos criar variáveis de variáveis
+
+$x = "nome"; // valor de nome
+
+echo "$x <br>";
+
+$$x = "lele"; // var com o nome x (nome), com valor lele
+
+echo "$nome <br>";
+
---
+++ VARIAVEIS/var_por_ref/index.php
@@ -0,0 +1,31 @@
+<?php
+
+// Podemos criar variáveis por referência;
+
+$x = 10;
+$y =& $x;
+
+echo $x;
+echo "<br>";
+echo $y;
+echo "<br>";
+
+$y = 15;
+
+echo "<br>";
+echo "Atribuição após ref";
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+echo "<br>";
+
+$x = 20;
+
+echo "<br>";
+echo "Atribuição após segunda ref";
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+echo "<br>";
