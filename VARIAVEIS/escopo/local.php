ae8b460465611caf95c243c5fa8d2be502a76eba
Letícia Bittencourt Vieira <bittencourt.vieira@academico.ifg.edu.br>
2024-07-15 17:59:40 -0300

    commit do segundo curso de php

---
+++ VARIAVEIS/escopo/local.php
@@ -0,0 +1,25 @@
+<?php 
+
+$x = 10;
+
+echo "$x global <br>";
+
+function teste() {
+    $x = 5;
+    
+    echo "$x local <br>";
+}
+
+teste();
+
+function testando() {
+    $x = 12;
+
+    echo "$x local 2 <br>";
+}
+
+echo "<br>";
+testando();
+teste();
+echo "$x global <br>";
+
